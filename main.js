fa7810bfd29c42d2ddbbfa64ab3a37e3a6bb8297
areum514 <21500514@handong.edu>
2019-08-14 10:48:36 +0900

    setting mysql mo

--- main.js
+++ main.js
@@ -5,14 +5,21 @@ var qs = require('querystring');
 var template = require('./lib/template.js');
 var path = require('path');
 var sanitizeHtml = require('sanitize-html');
-
+var mysql      = require('mysql');
+var db=mysql.createConnection({
+  host     : 'localhost',
+  user     : 'nodejs',
+  password : '1004',
+  database : 'opentutorials'
+});
+db.connect();
 var app = http.createServer(function(request,response){
     var _url = request.url;
     var queryData = url.parse(_url, true).query;
     var pathname = url.parse(_url, true).pathname;
     if(pathname === '/'){
       if(queryData.id === undefined){
-        fs.readdir('./data', function(error, filelist){
+        /*fs.readdir('./data', function(error, filelist){
           var title = 'Welcome';
           var description = 'Hello, Node.js';
           var list = template.list(filelist);
@@ -22,6 +29,10 @@ var app = http.createServer(function(request,response){
           );
           response.writeHead(200);
           response.end(html);
+        });*/
+        db.query(`SELECT * FROM topic`,function (error,topic){
+          response.writeHead(200);
+          response.end('success');
         });
       } else {
         fs.readdir('./data', function(error, filelist){
